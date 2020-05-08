03e73e0f6425ab4d868a222b5245930006b9c0e4
Derek <dereknugroho@gmail.com>
2020-05-08 23:37:04 +0000

    Begin build for PantryList component

---
+++ src/components/Pantry/PantryList/PantryList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import PantryListItem from './PantryListItem';
+
+export default function PantryList(props) {
+  // Retrieve list from database
+  // First item in list will always be New/Form
+  // Render PantryListItem in each iteration
+
+  return (
+    <ul>
+      
+    </ul>
+  );
+};
