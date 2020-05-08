6c2cee7eb75c12ea7a3660e6dcfb7f0c0f409744
Derek <dereknugroho@gmail.com>
2020-05-08 21:46:29 +0000

    Test Show component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Show from './components/Pantries/Show';
+
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
 					Pot Lucky
 				</a>
 			</header>
+			<Show />
 		</div>
 	);
 }
