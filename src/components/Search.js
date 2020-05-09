eddcc51a9e288c8ea3844d727f3b2dc87ca4ea8e
Kelsey Griffin <kjulegriffin@gmail.com>
2020-05-09 14:25:06 -0400

    create search component

---
+++ src/components/Search.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Search(props) {
+  return (
+    <div>
+      Search:
+      <form><input placeholder='input search'></input></form> 
+    </div>
+  );
+}
