997a105a46cad574a7325e3898d9844c92c57ba7
Derek <dereknugroho@gmail.com>
2020-05-10 23:10:23 +0000

    Remove hard-coded state and API GET request test

--- src/components/App.js
+++ src/components/App.js
@@ -12,85 +12,6 @@ import Search from './Search';
 import NavBar from './NavBar';
 import Filter from './Filter';
 
-const state = {
-	pantryListItems : [
-		{
-			id       : 1,
-			user_id  : 1,
-			name     : 'banana',
-			quantity : 3.432,
-			unit     : 'pieces',
-			expiry   : '2020-05-08'
-		},
-		{
-			id       : 2,
-			user_id  : 1,
-			name     : 'sugar',
-			quantity : 23.0,
-			unit     : 'ounces',
-			expiry   : '2020-05-08'
-		},
-		{
-			id       : 3,
-			user_id  : 1,
-			name     : 'turnip',
-			quantity : 12.0,
-			unit     : 'pieces',
-			expiry   : '2020-05-08'
-		},
-		{
-			id       : 4,
-			user_id  : 2,
-			name     : 'lemon juice',
-			quantity : 0.75,
-			unit     : 'cups',
-			expiry   : '2020-05-09'
-		},
-		{
-			id       : 5,
-			user_id  : 2,
-			name     : 'cinnamon',
-			quantity : 1.95,
-			unit     : 'tablespoons',
-			expiry   : '2020-05-09'
-		},
-		{
-			id       : 6,
-			user_id  : 3,
-			name     : 'salt',
-			quantity : 1.0,
-			unit     : 'pinch',
-			expiry   : '2020-05-09'
-		}
-	],
-	recipeListItems : [
-		{
-			id             : 633508,
-			image          : 'Baked-Cheese-Manicotti-633508.jpg',
-			imageUrls      : [ 'Baked-Cheese-Manicotti-633508.jpg' ],
-			readyInMinutes : 45,
-			servings       : 6,
-			title          : 'Baked Cheese Manicotti'
-		},
-		{
-			id             : 634873,
-			image          : 'Best-Baked-Macaroni-and-Cheese-634873.jpg',
-			imageUrls      : [ 'Best-Baked-Macaroni-and-Cheese-634873.jpg' ],
-			readyInMinutes : 45,
-			servings       : 12,
-			title          : 'Best Baked Macaroni and Cheese'
-		}
-	],
-	users           : [
-		{
-			id       : 1,
-			name     : 'Harry Potter',
-			email    : 'hp@hogwarts.com(opens in new tab)',
-			password : 'password' //will store as hash
-		}
-	]
-};
-
 function App() {
 	// const [ users, setUsers ] = useState([]);
 
@@ -99,14 +20,7 @@ function App() {
 
 	useEffect(() => {
 		getPantry();
-		// getRecipeList();
 	}, []);
-
-	function getRecipeList() {
-		axios.get('https://api.spoonacular.com/recipes/search?query=cheese&number=10&apiKey=4ed5da45f1c94518a9663b95f895c3b3')
-		  .then(res => setRecipeList(res.data.results))
-		  .catch(err => console.error(err));
-	}
 
 	function getPantry() {
 		fetch('http://localhost:3001')
