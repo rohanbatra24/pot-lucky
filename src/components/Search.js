bb6e4edb710937d63de12c32d1cd5c1c23b47f18
Kelsey Griffin <kjulegriffin@gmail.com>
2020-05-11 13:49:29 -0400

    fix what second api call returns

--- src/components/Search.js
+++ src/components/Search.js
@@ -10,12 +10,16 @@ export default function Search(props) {
     //   .then(res => props.setRecipeList(res.data.results))
     //   .catch(err => console.error(err))
 
-    searchText && axios.get(`https://api.spoonacular.com/recipes/search?query=${searchText}&number=5&apiKey=4ed5da45f1c94518a9663b95f895c3b3`)
+    searchText && axios.get(`https://api.spoonacular.com/recipes/search?query=${searchText}&number=5&apiKey=7a707a8f3c6b42ffb52bccfa111f4a00`)
     .then(res => res.data.results.map(item => item.id ))
     .then(ids => {
       console.log("ids --->", ids)
-      console.log("join ids ---> ", ids.join(','))
-      return axios.get(`https://api.spoonacular.com/recipes/informationBulk?ids=${ids.join(',')}&apiKey=4ed5da45f1c94518a9663b95f895c3b3`)})
+      console.log("join ids ---> ", ids.join('%2C'))
+      ids && axios.get(`https://api.spoonacular.com/recipes/informationBulk?ids=${ids.join(',')}&apiKey=7a707a8f3c6b42ffb52bccfa111f4a00`)})
+    .then(res => {
+      console.log("RESULTS===> ", res.data)
+      props.setRecipeList(res.data.results)
+    })
     .catch(err => console.error(err))
   }, [searchText]);
 
