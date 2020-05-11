0456561f0867d4c24d3191ae69b79b37cd94caf4
Kelsey Griffin <kjulegriffin@gmail.com>
2020-05-11 13:19:45 -0400

    write second api call to get bulk information

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,15 +6,23 @@ export default function Search(props) {
 
   // Dynamically update list of recipes based on user's search input
   useEffect(() => {
+    // searchText && axios.get(`https://api.spoonacular.com/recipes/search?query=${searchText}&number=5&apiKey=4ed5da45f1c94518a9663b95f895c3b3`)
+    //   .then(res => props.setRecipeList(res.data.results))
+    //   .catch(err => console.error(err))
+
     searchText && axios.get(`https://api.spoonacular.com/recipes/search?query=${searchText}&number=5&apiKey=4ed5da45f1c94518a9663b95f895c3b3`)
-      .then(res => props.setRecipeList(res.data.results))
-      .catch(err => console.error(err))
+    .then(res => res.data.results.map(item => item.id ))
+    .then(ids => {
+      console.log("ids --->", ids)
+      console.log("join ids ---> ", ids.join(','))
+      return axios.get(`https://api.spoonacular.com/recipes/informationBulk?ids=${ids.join(',')}&apiKey=4ed5da45f1c94518a9663b95f895c3b3`)})
+    .catch(err => console.error(err))
   }, [searchText]);
 
   return (
     <div>
       Search:
-      <form>
+      <form class="search">
         <input
           placeholder='input search'
           value={searchText}
