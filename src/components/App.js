6811a43f95bac8e11ff1f6c270d3810e333571d9
Rohan Batra <rohanbatra24@gmail.com>
2020-05-11 22:35:05 +0000

    add selected pantry itmes for recipe search

--- src/components/App.js
+++ src/components/App.js
@@ -14,33 +14,34 @@ import Filter from './Filter';
 
 function App() {
 	// const [ users, setUsers ] = useState([]);
-	const [filters, setFilters] = useState({
-    vegan: false,
-    vegetarian: false,
-    glutenfree: false,
-    healthy: false,
-    time: 0,
-    rating: 0,
-    cuisine: null,
-    dish: null
-  })
-	const [pantry, setPantry] = useState([]);
-	const [recipeList, setRecipeList] = useState([]);
+	const [ filters, setFilters ] = useState({
+		vegan      : false,
+		vegetarian : false,
+		glutenfree : false,
+		healthy    : false,
+		time       : 0,
+		rating     : 0,
+		cuisine    : null,
+		dish       : null
+	});
+	const [ pantry, setPantry ] = useState([]);
+	const [ recipeList, setRecipeList ] = useState([]);
+	const [ selectedPantryList, setSelectedPantryList ] = useState({});
 
 	useEffect(() => {
-		console.log("about to get pantry")
+		// console.log("about to get pantry")
 		getPantry();
 	}, []);
 
-	function getRecipes() {	
-		console.log("inside getRecipes()")
-		console.log("recipeList ====>", recipeList)
+	function getRecipes() {
+		// console.log("inside getRecipes()")
+		// console.log("recipeList ====>", recipeList)
 		//loop filters keys, get true ones "active filters"
 		// const activeFilters = Object.keys(filters).filters(f => f)
-		const filtered = recipeList.filter(item => item.vegan)
+		const filtered = recipeList.filter((item) => !item.vegan);
 		const recipes = filtered.map((item) => {
 			return <RecipeList name={item.title} />;
-		})
+		});
 		// const recipes = recipeList.map((item) => {
 		// 	for (let filter in filters) {
 		// 		if (item[filter] === filters.filter) {
@@ -51,26 +52,47 @@ function App() {
 		return recipes;
 	}
 
+	function getSelectedPantryList() {
+		setSelectedPantryList({ ...selectedPantryList, [data]: true });
+	}
 
 	function getPantry() {
 		// fetch('http://localhost:3001')
 		fetch('http://localhost:8080')
 			.then((response) => {
-				console.log('response (fetch from localhost) ====>', response);
+				// console.log('response (fetch from localhost) ====>', response);
 				return response.json();
 			})
 			.then((data) => {
 				console.log('data (json from fetch from localhost) ====>', data);
 				setPantry(data);
 			})
-			.catch(err => console.error(err));
+			.catch((err) => console.error(err));
 	}
 
-	console.log('pantry ====>', pantry);
+	// console.log('pantry ====>', pantry);
+
+	// const addPantryButtonHandler = () => {
+	// 	alert(this);
+	// };
 
 	const pantryList = pantry.map((item) => {
-		return <h1>{item.name}</h1>;
+		return (
+			<Fragment>
+				<h1>{item.name}</h1>
+				<button
+					onClick={() => setSelectedPantryList({ ...selectedPantryList, [item.name]: true })}
+					data-id={item.name}
+				>
+					+
+				</button>
+			</Fragment>
+		);
 	});
+
+	console.log(selectedPantryList);
+
+	console.log('===', pantryList);
 
 	return (
 		<Fragment>
@@ -90,8 +112,13 @@ function App() {
 					<div className="recipe-container">
 						<Search setRecipeList={setRecipeList} />
 						{/* {recipeList.length > 0 && // only show filters if there are recipes */}
-							<Filter filters={filters} setFilters={setFilters} dishes={['breakfast', 'dinner']} cuisines={['italian', 'mediterranian']} />
-      			{/* } */}
+						<Filter
+							filters={filters}
+							setFilters={setFilters}
+							dishes={[ 'breakfast', 'dinner' ]}
+							cuisines={[ 'italian', 'mediterranian' ]}
+						/>
+						{/* } */}
 						<label htmlFor="">
 							<h1>Recipes</h1>
 						</label>
