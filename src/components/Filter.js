e5ba9b990c8394ab535583375e2d4694ae3ddb52
Kelsey Griffin <kjulegriffin@gmail.com>
2020-05-12 10:25:34 -0400

    update dropdown and slider filters

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Form from 'react-bootstrap/Form';
 import Switch from './Switch';
+import Slider from './Slider';
 
 // cuisine - dropdown
 // time required - slider
@@ -13,17 +14,33 @@ import Switch from './Switch';
 // dish type - dropdown
 
 export default function Filter(props) {
+  
+  //get a list of dish types from recipe list
+  //to form dish type dropdown 
+  const dishList = new Set()
+  
+  props.recipeList.forEach( recipe => {
+    recipe.dishTypes.forEach( dish => {
+      dishList.add(dish);
+    })
+  });
 
-  const dishesList = props.dishes.map( dish => {
-    return <option>${dish}</option>
-  })
+  const dishesList = [...dishList].map( type => {
+    return <option value={type}>{type}</option>;
+  });
 
-  const cuisinesList = props.cuisines.map( type => {
-    return <option>${type}</option>
-  })
+  //for time range slider
+  // go through recipeList and get readyInMinutes
+  //choose the maximum and the minimum
+  const times = [];
+  props.recipeList.forEach( recipe => {
+    times.push(recipe.readyInMinutes);
+  });
+  const max_time= Math.max(...times);
+  const min_time= Math.min(...times);
+  console.log("max: ", max_time, "min: ", min_time);
 
-  console.log("Filters ====>", props.filters)
-
+  //////////
   return (
     <div className='filters'>
     <Form>
@@ -47,34 +64,25 @@ export default function Filter(props) {
         handleToggle={() => props.setFilters({...props.filters, healthy: !props.filters.healthy})}
       />
       Healthy
-      <Form.Group controlId="formBasicRange">
-        <Form.Label>Time</Form.Label>
-        <Form.Control type="range" />
-      </Form.Group>
-      <Form.Group controlId="formBasicRange">
-        <Form.Label>Rating</Form.Label>
-        <Form.Control type="range" />
-      </Form.Group>
-      <Form.Group controlId="exampleForm.SelectCustom">
-      <Form.Label>Cuisine</Form.Label>
-        <Form.Control as="select" custom>
-          {cuisinesList}
-          {/* <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option> */}
-        </Form.Control>
-      </Form.Group>
+      <Slider 
+        maxSize={100}
+        minSize={0}
+        initialSize={props.filters.rating}
+        handleChange={(event) => props.setFilters({...props.filters, rating: event.target.value})}
+      />
+      Rating
+      <Slider 
+        maxSize={max_time}
+        minSize={min_time}
+        initialSize={props.filters.time}
+        handleChange={(event) => props.setFilters({...props.filters, time: event.target.value})}
+      />
+      Time
       <Form.Group controlId="exampleForm.SelectCustom">
       <Form.Label>Dish Type</Form.Label>
-        <Form.Control as="select" custom>
+        <Form.Control as="select" custom onChange={(event) => props.setFilters({...props.filters, dish: event.target.value})}>
+          <option>Choose a Dish Type</option>
           {dishesList}
-          {/* <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option> */}
         </Form.Control>
       </Form.Group>
     </Form>
