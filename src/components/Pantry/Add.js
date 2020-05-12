48b399d12da4d336718894e6c50ee61776afe496
Kelsey Griffin <kjulegriffin@gmail.com>
2020-05-12 17:44:08 -0400

    change input types, add expiry input

--- src/components/Pantry/Add.js
+++ src/components/Pantry/Add.js
@@ -5,15 +5,16 @@ export default function Add(props) {
     name: "",
     quantity: "",
     unit: "",
+    expiry: ""
   }) 
   return (
     <>
       <h3>Add new item to pantry</h3>
       <form onSubmit={(event) => props.handleAddItem(event, values)}>
-        <input value={values.name} name="name" placeholder="Name" type="text" onChange={(e) => setValues({...values, name: e.target.value})}></input>
-        <input value={values.quantity} name="quantity" placeholder="Quantity" type="text" onChange={(e) => setValues({...values, quantity: e.target.value})}></input>
+        <input value={values.name} name="name" placeholder="Name" type="text" onChange={(e) => setValues({...values, name: e.target.value})}></input> {/* dropdown */}
+        <input value={values.quantity} name="quantity" placeholder="Quantity" type="number" onChange={(e) => setValues({...values, quantity: e.target.value})}></input>
         <input value={values.unit} name="unit" placeholder="Unit" type="text" onChange={(e) => setValues({...values, unit: e.target.value})}></input> {/* dropdown */}
-        <input value={values.expiry} name="expiry" placeholder="Expiry" type="text" onChange={(e) => setValues({...values, expiry: e.target.value})}></input> {/* calender input? */}
+        <input value={values.expiry} name="expiry" placeholder="Expiration Date (Optional)" type="date" onChange={(e) => setValues({...values, expiry: e.target.value})}></input>
         <button type="submit">Add</button>
       </form> 
     </>
