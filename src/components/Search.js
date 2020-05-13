2122e184f6fe8567325200ca8f9b97728dc2e0db
Kelsey Griffin <kjulegriffin@gmail.com>
2020-05-13 11:32:50 -0400

    make api key variable and add extra api keys

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,6 +3,20 @@ import axios from 'axios';
 import bulkInfo from '../assets/api_bulk_info';
 import getBulkInfo from '../assets/api_bulk_info';
 
+// API KEYS 
+// 7a707a8f3c6b42ffb52bccfa111f4a00
+// 4ed5da45f1c94518a9663b95f895c3b3
+// 692296bbb58c433b89dba0eb2c54099b
+// dc75b7fa4f7948ceae6ce8c4e008208e
+// b183b79d7863488aaca6c2aeb086183b
+// 76961f7e03aa4b47955a3ce3c98c45f6 {{{IN-USE}}}
+// a59c64c98d4d4156b1e8d2992bc5ca06
+// 0939358abf5240f598df923e8a915144
+// a260062916e04970801d03d0db2c32b4
+// 46ef327a373243b192fc86af3fa94823
+
+
+const apiKey = '76961f7e03aa4b47955a3ce3c98c45f6'
 export default function Search(props) {
 	const [ searchText, setSearchText ] = useState('');
 
@@ -13,7 +27,7 @@ export default function Search(props) {
 		searchText &&
 			axios
 				.get(
-					`https://api.spoonacular.com/recipes/search?query=${searchText}&number=10&apiKey=dc75b7fa4f7948ceae6ce8c4e008208e`
+					`https://api.spoonacular.com/recipes/search?query=${searchText}&number=10&apiKey=${apiKey}`
 				)
 				.then((res) => res.data.results.map((item) => item.id))
 				.then((ids) => {
@@ -22,7 +36,7 @@ export default function Search(props) {
 							.get(
 								`https://api.spoonacular.com/recipes/informationBulk?ids=${ids.join(
 									','
-								)}&apiKey=dc75b7fa4f7948ceae6ce8c4e008208e`
+								)}&apiKey=${apiKey}`
 							)
 							.then((res) => {
 								console.log('RESULTS from api call==>', res.data);
@@ -49,7 +63,7 @@ export default function Search(props) {
 
 		axios
 			.get(
-				`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${apiString}&apiKey=dc75b7fa4f7948ceae6ce8c4e008208e`
+				`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${apiString}&apiKey=${apiKey}`
 			)
 			.then((res) => {
 				console.log(res.data);
@@ -62,7 +76,7 @@ export default function Search(props) {
 						.get(
 							`https://api.spoonacular.com/recipes/informationBulk?ids=${ids.join(
 								','
-							)}&apiKey=dc75b7fa4f7948ceae6ce8c4e008208e`
+							)}&apiKey=${apiKey}`
 						)
 						.then((res) => {
 							console.log('RESULTS from api call==>', res.data);
