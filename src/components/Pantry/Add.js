3f9670fc8b2ec03bf501c8cb784222b126d04be6
Kelsey Griffin <kjulegriffin@gmail.com>
2020-05-13 17:24:29 -0400

    use ingredients as input dropdown list

--- src/components/Pantry/Add.js
+++ src/components/Pantry/Add.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Add(props) {
   const [values, setValues] = useState({
@@ -7,19 +7,29 @@ export default function Add(props) {
     unit: "",
     expiry: ""
   }) 
+  
+  const ingredientList = props.ingredients.map(ingred => {
+    console.log("ingred ====> ", ingred)
+    return <option>{ingred.name}</option>
+  })
+  
+  console.log("ingred list ====> ", ingredientList)
   return (
     <>
       <h3>Add new item to pantry</h3>
       <form onSubmit={(event) => props.handleAddItem(event, values)}>
         <div>
-          <input type="text" list="ingredients" />
+          <input value={values.name} 
+                 name="name"
+                 placeholder="Ingredient" 
+                 type="text" 
+                 list="ingredients" 
+                 onChange={(e) => setValues({...values, name: e.target.value})}>
+          </input>
           <datalist id="ingredients">
-            <option>Apricot</option>
-            <option>Apple</option>
-            <option>Applesauce</option>
+            <select>{ingredientList}</select>
           </datalist>
         </div>
-        <input value={values.name} name="name" placeholder="Name" type="text" onChange={(e) => setValues({...values, name: e.target.value})}></input> {/* dropdown */}
         <input value={values.quantity} name="quantity" placeholder="Quantity" type="number" onChange={(e) => setValues({...values, quantity: e.target.value})}></input>
         <input value={values.unit} name="unit" placeholder="Unit" type="text" onChange={(e) => setValues({...values, unit: e.target.value})}></input> {/* dropdown */}
         <input value={values.expiry} name="expiry" placeholder="Expiration Date (Optional)" type="date" onChange={(e) => setValues({...values, expiry: e.target.value})}></input>
