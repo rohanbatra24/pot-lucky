b970a45f648669f5db22008625bcdfd43a55357a
Kelsey Griffin <kjulegriffin@gmail.com>
2020-05-13 15:16:51 -0400

    sketch out datalist for add ingredient to pantry

--- src/components/Pantry/Add.js
+++ src/components/Pantry/Add.js
@@ -11,6 +11,14 @@ export default function Add(props) {
     <>
       <h3>Add new item to pantry</h3>
       <form onSubmit={(event) => props.handleAddItem(event, values)}>
+        <div>
+          <input type="text" list="ingredients" />
+          <datalist id="ingredients">
+            <option>Apricot</option>
+            <option>Apple</option>
+            <option>Applesauce</option>
+          </datalist>
+        </div>
         <input value={values.name} name="name" placeholder="Name" type="text" onChange={(e) => setValues({...values, name: e.target.value})}></input> {/* dropdown */}
         <input value={values.quantity} name="quantity" placeholder="Quantity" type="number" onChange={(e) => setValues({...values, quantity: e.target.value})}></input>
         <input value={values.unit} name="unit" placeholder="Unit" type="text" onChange={(e) => setValues({...values, unit: e.target.value})}></input> {/* dropdown */}
