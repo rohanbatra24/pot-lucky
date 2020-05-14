4d883e9d5053d21942427d7a7268631aa0c634d0
Rohan Batra <rohanbatra@Rohans-MacBook-Pro.local>
2020-05-14 11:41:36 -0400

    format with prettier

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,7 +9,7 @@ import getBulkInfo from '../assets/api_bulk_info';
 // 692296bbb58c433b89dba0eb2c54099b
 // dc75b7fa4f7948ceae6ce8c4e008208e
 // b183b79d7863488aaca6c2aeb086183b
-// 76961f7e03aa4b47955a3ce3c98c45f6 
+// 76961f7e03aa4b47955a3ce3c98c45f6
 // a59c64c98d4d4156b1e8d2992bc5ca06
 // 0939358abf5240f598df923e8a915144
 // a260062916e04970801d03d0db2c32b4
@@ -57,22 +57,25 @@ export default function Search(props) {
 
 		//api.spoonacular.com/recipes/findByIngredients?ingredients=banana,+sugar,+turnip&apiKey=692296bbb58c433b89dba0eb2c54099b
 
-		// search by ingredients 
+		// search by ingredients
 		// params: ranking=2 (minimize missing ingredients)
 		//         ignorePantry=true (ignore common pantry items like water, flour)
 		axios
-			.get(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${apiString}&apiKey=${apiKey}&ignorePantry=true&ranking=2`)
+			.get(
+				`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${apiString}&apiKey=${apiKey}&ignorePantry=true&ranking=2`
+			)
 			.then((res) => {
-				console.log("first log from generate: ", res.data);
-				const noMissingIngred = res.data.filter(recipe => {
-					console.log("recipe =====> ", recipe.missedIngredientCount)
-					return recipe.missedIngredientCount < 1
-				})
+				console.log('first log from generate: ', res.data);
+				const noMissingIngred = res.data.filter((recipe) => {
+					console.log('recipe =====> ', recipe.missedIngredientCount);
+					return recipe.missedIngredientCount < 1;
+				});
 				if (noMissingIngred.length) {
 					return noMissingIngred.map((item) => item.id);
-				} else {
-					props.setRecipeList([])
-					throw new Error("Oops! Need to add more items to your search. No recipes found.")
+				}
+				else {
+					props.setRecipeList([]);
+					throw new Error('Oops! Need to add more items to your search. No recipes found.');
 				}
 			})
 			.then((ids) => {
