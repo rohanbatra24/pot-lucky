b05ca747405ff83b532e8294015f346ecf953769
Kelsey Griffin <kjulegriffin@gmail.com>
2020-05-14 17:10:44 -0400

    use bootstrap accordian for pantrylist

--- src/components/Pantry/PantryList.js
+++ src/components/Pantry/PantryList.js
@@ -4,30 +4,45 @@ import Add from './Add';
 import ListGroup from 'react-bootstrap/ListGroup';
 import './styles.css';
 
+
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+
 export default function PantryList(props) {
 	// Retrieve list from database
 	// First item in list will always be New/Form
 	// Render PantryListItem in each iteration
 
+
+
 	const pantryList = props.pantry.map((item) => {
 		return (
-			<Fragment>
-				<Show
-					setSelectedPantryList={props.setSelectedPantryList}
-					selectedPantryList={props.selectedPantryList}
-					item={item}
-					handleDeleteItem={props.handleDeleteItem}
-				/>
-				{/* fix needed = only allow user to add item once */}
-			</Fragment>
-		);
-	});
-
+				<Card>
+					<Accordion.Toggle as={Card.Header} eventKey={item.id}>
+						{item.name}
+					</Accordion.Toggle>
+					<Accordion.Collapse eventKey={item.id}>
+						<Card.Body>
+							<Show 
+								setSelectedPantryList={props.setSelectedPantryList}
+								selectedPantryList={props.selectedPantryList}
+								item={item}
+								handleDeleteItem={props.handleDeleteItem}
+							/>
+						</Card.Body>
+					</Accordion.Collapse>
+				</Card>
+			)
+	})	
+		
 	return (
 		<Fragment>
 			<Add ingredients={props.ingredients} handleAddItem={props.handleAddItem} />
-
-			<ListGroup as="ul">{pantryList}</ListGroup>
+			<Accordion defaultActiveKey="0">
+				{pantryList}
+			</Accordion>
+			
+			{/* <ListGroup as="ul">{pantryList}</ListGroup> */}
 		</Fragment>
 	);
 }
