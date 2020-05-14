ce50e31d2a2ac1907c99ac8198fd5600c6efd0a7
Kelsey Griffin <kjulegriffin@gmail.com>
2020-05-13 22:42:10 -0400

    add logic to only show recipes with no missing ing

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,13 +9,13 @@ import getBulkInfo from '../assets/api_bulk_info';
 // 692296bbb58c433b89dba0eb2c54099b
 // dc75b7fa4f7948ceae6ce8c4e008208e
 // b183b79d7863488aaca6c2aeb086183b
-// 76961f7e03aa4b47955a3ce3c98c45f6 {{{IN-USE}}}
+// 76961f7e03aa4b47955a3ce3c98c45f6 
 // a59c64c98d4d4156b1e8d2992bc5ca06
 // 0939358abf5240f598df923e8a915144
 // a260062916e04970801d03d0db2c32b4
-// 46ef327a373243b192fc86af3fa94823
+// 46ef327a373243b192fc86af3fa94823 {{{IN-USE}}}
 
-const apiKey = 'a59c64c98d4d4156b1e8d2992bc5ca06';
+const apiKey = '46ef327a373243b192fc86af3fa94823';
 export default function Search(props) {
 	const [ searchText, setSearchText ] = useState('');
 
@@ -40,7 +40,6 @@ export default function Search(props) {
 								props.setRecipeList(res.data);
 							});
 					}
-					g;
 				})
 				.catch((err) => console.error(err));
 	}
@@ -58,22 +57,32 @@ export default function Search(props) {
 
 		//api.spoonacular.com/recipes/findByIngredients?ingredients=banana,+sugar,+turnip&apiKey=692296bbb58c433b89dba0eb2c54099b
 
+		// search by ingredients 
+		// params: ranking=2 (minimize missing ingredients)
+		//         ignorePantry=true (ignore common pantry items like water, flour)
 		axios
-			.get(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${apiString}&apiKey=${apiKey}`)
+			.get(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${apiString}&apiKey=${apiKey}&ignorePantry=true&ranking=2`)
 			.then((res) => {
-				console.log(res.data);
-				return res.data.map((item) => item.id);
+				console.log("first log from generate: ", res.data);
+				const noMissingIngred = res.data.filter(recipe => {
+					console.log("recipe =====> ", recipe.missedIngredientCount)
+					return recipe.missedIngredientCount < 1
+				})
+				if (noMissingIngred.length) {
+					return noMissingIngred.map((item) => item.id);
+				} else {
+					props.setRecipeList([])
+					throw new Error("Oops! Need to add more items to your search. No recipes found.")
+				}
 			})
 			.then((ids) => {
-				console.log(ids);
 				if (ids) {
 					return axios
 						.get(
 							`https://api.spoonacular.com/recipes/informationBulk?ids=${ids.join(',')}&apiKey=${apiKey}`
 						)
 						.then((res) => {
-							console.log('RESULTS from api call==>', res.data);
-							console.log(res);
+							console.log('RESULTS from BULK api call==>', res.data);
 							props.setRecipeList(res.data);
 						});
 				}
