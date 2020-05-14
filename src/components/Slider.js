d72296cdb69d5575feee30a420170edc5b03e417
Rohan Batra <rohanbatra@Rohans-MacBook-Pro.local>
2020-05-14 19:26:29 -0400

    add divs for styling

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,28 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const Slider = ({ initialSize, minSize, maxSize, handleChange, type }) => {
-  
-  
-  return (
-    <>
-      <input
-        value={initialSize}
-        onChange={handleChange}
-        max={maxSize}
-        min={minSize}
-        className="slider"
-        id={`${type}-slider`}
-        type="range"
-      />
-      <label
-        className="react-slider-label"
-        htmlFor={`react-slider`}
-      >
-        <span className={`react-slider`} />
-      </label>
-      <output for={`${type}-slider`} id={`${type}-slider-output`}>{initialSize}</output>
-    </>
-  );
+	return (
+		<div className="sliderFilterContainer-item">
+			<label className="react-slider-label" htmlFor={`react-slider`}>
+				<span className={`react-slider`}>{type}</span>
+			</label>
+			<input
+				value={initialSize}
+				onChange={handleChange}
+				max={maxSize}
+				min={minSize}
+				className="slider"
+				id={`${type}-slider`}
+				type="range"
+			/>
+
+			<output for={`${type}-slider`} id={`${type}-slider-output`}>
+				{initialSize}
+			</output>
+		</div>
+	);
 };
 
 export default Slider;
