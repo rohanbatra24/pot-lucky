9e590bbe7ef4d738c271efd59cc8519282bb71c3
Derek <dereknugroho@gmail.com>
2020-05-14 02:04:32 +0000

    Add basic styling

--- src/components/Pantry/Add.js
+++ src/components/Pantry/Add.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Form, Button } from 'react-bootstrap';
 
 export default function Add(props) {
   const [values, setValues] = useState({
@@ -9,32 +10,36 @@ export default function Add(props) {
   }) 
   
   const ingredientList = props.ingredients.map(ingred => {
-    console.log("ingred ====> ", ingred)
+    // console.log("ingred ====> ", ingred)
     return <option>{ingred.name}</option>
   })
   
-  console.log("ingred list ====> ", ingredientList)
+  // console.log("ingred list ====> ", ingredientList)
   return (
     <>
-      <h3>Add new item to pantry</h3>
-      <form onSubmit={(event) => props.handleAddItem(event, values)}>
-        <div>
-          <input value={values.name} 
-                 name="name"
-                 placeholder="Ingredient" 
-                 type="text" 
-                 list="ingredients" 
-                 onChange={(e) => setValues({...values, name: e.target.value})}>
-          </input>
+      <Form>
+        <Form.Group controlId="formBasicEmail" onSubmit={(event) => props.handleAddItem(event, values)}>
+          <Form.Label>Add new item to pantry</Form.Label>
+          <Form.Control
+            class="form-component"
+            value={values.name} 
+            name="name"
+            placeholder="Ingredient" 
+            type="text" 
+            list="ingredients" 
+            onChange={(e) => setValues({...values, name: e.target.value})}
+          />
+            
           <datalist id="ingredients">
             <select>{ingredientList}</select>
           </datalist>
-        </div>
-        <input value={values.quantity} name="quantity" placeholder="Quantity" type="number" onChange={(e) => setValues({...values, quantity: e.target.value})}></input>
-        <input value={values.unit} name="unit" placeholder="Unit" type="text" onChange={(e) => setValues({...values, unit: e.target.value})}></input> {/* dropdown */}
-        <input value={values.expiry} name="expiry" placeholder="Expiration Date (Optional)" type="date" onChange={(e) => setValues({...values, expiry: e.target.value})}></input>
-        <button type="submit">Add</button>
-      </form> 
+
+          <Form.Control class="form-component" value={values.quantity} name="quantity" placeholder="Quantity" type="number" onChange={(e) => setValues({...values, quantity: e.target.value})} />
+          <Form.Control class="form-component" value={values.unit} name="unit" placeholder="Unit" type="text" onChange={(e) => setValues({...values, unit: e.target.value})} /> {/* dropdown */}
+          <Form.Control class="form-component" value={values.expiry} name="expiry" placeholder="Expiration Date (Optional)" type="date" onChange={(e) => setValues({...values, expiry: e.target.value})} />
+          <Button variant="info" type="submit">Add</Button>
+        </Form.Group> 
+      </Form>
     </>
   )
   
