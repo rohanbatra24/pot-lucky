b50ff08c42178ac76938e13ede405c9a45ed2ace
Rohan Batra <rohanbatra@Rohans-MacBook-Pro.local>
2020-05-14 19:26:43 -0400

    add divs for styling

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -2,23 +2,20 @@ import React from 'react';
 // import './Switch.css';
 
 const Switch = ({ isOn, handleToggle, type }) => {
-  return (
-    <div className="custom-control custom-switch">
-      <input 
-        checked={isOn}
-        onChange={handleToggle}
-        className="custom-control-input"
-        id={`${type}-switch`}
-        type="checkbox"
-      />
-      <label
-        className="custom-control-label"
-        htmlFor={`${type}-switch`}
-      >
-        <span className={`react-switch-button`} />
-      </label>
-    </div>
-  );
+	return (
+		<div className="custom-control custom-switch">
+			<input
+				checked={isOn}
+				onChange={handleToggle}
+				className="custom-control-input"
+				id={`${type}-switch`}
+				type="checkbox"
+			/>
+			<label className="custom-control-label" htmlFor={`${type}-switch`}>
+				<span className={`react-switch-button`}>{type}</span>
+			</label>
+		</div>
+	);
 };
 
 export default Switch;
