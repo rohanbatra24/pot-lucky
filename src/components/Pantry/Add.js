1edf456aded2ec7dc449688b24f6b34ba7205667
Rohan Batra <rohanbatra@Rohans-MacBook-Pro.local>
2020-05-14 11:42:10 -0400

    create units dropdown & hardcode values

--- src/components/Pantry/Add.js
+++ src/components/Pantry/Add.js
@@ -1,45 +1,105 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
 export default function Add(props) {
-  const [values, setValues] = useState({
-    name: "",
-    quantity: "",
-    unit: "",
-    expiry: ""
-  }) 
-  
-  const ingredientList = props.ingredients.map(ingred => {
-    return <option>{ingred.name}</option>
-  })
-  
-  // console.log("ingred list ====> ", ingredientList)
-  return (
-    <>
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Add new item to pantry</Form.Label>
-          <Form.Control
-            className="form-component"
-            value={values.name} 
-            name="name"
-            placeholder="Ingredient" 
-            type="text" 
-            list="ingredients" 
-            onChange={(e) => setValues({...values, name: e.target.value})}
-          />
-            
-          <datalist id="ingredients">
-            <select>{ingredientList}</select>
-          </datalist>
+	const [ values, setValues ] = useState({
+		name     : '',
+		quantity : '',
+		unit     : '',
+		expiry   : ''
+	});
 
-          <Form.Control class="form-component" value={values.quantity} name="quantity" placeholder="Quantity" type="number" onChange={(e) => setValues({...values, quantity: e.target.value})} />
-          <Form.Control class="form-component" value={values.unit} name="unit" placeholder="Unit" type="text" onChange={(e) => setValues({...values, unit: e.target.value})} /> {/* dropdown */}
-          <Form.Control class="form-component" value={values.expiry} name="expiry" placeholder="Expiration Date (Optional)" type="date" onChange={(e) => setValues({...values, expiry: e.target.value})} />
-          <Button variant="info" type="submit" onClick={(event) => props.handleAddItem(event, values)}>Add</Button>
-        </Form.Group> 
-      </Form>
-    </>
-  )
-  
-};
+	const ingredientList = props.ingredients.map((ingred) => {
+		return <option>{ingred.name}</option>;
+	});
+
+	const units = [
+		'g',
+		'kg',
+		'lbs',
+		'pounds',
+		'ozs',
+		'cups',
+		'ml',
+		'l',
+		'liters',
+		'tsp',
+		'tbsps',
+		'qt',
+		'bunch',
+		'scoops',
+		'leaves',
+		'drops',
+		'sheets',
+		'slices',
+		'inches',
+		'stalks',
+		'sticks',
+
+		'strips',
+
+		'sprigs',
+
+		'dashes',
+
+		'pinches'
+	];
+
+	const unitList = units.map((unit) => {
+		return <option>{unit}</option>;
+	});
+
+	return (
+		<Fragment>
+			<Form>
+				<Form.Group controlId="formBasicEmail">
+					<Form.Label>Add new item to pantry</Form.Label>
+					<Form.Control
+						className="form-component"
+						value={values.name}
+						name="name"
+						placeholder="Ingredient"
+						type="text"
+						list="ingredients"
+						onChange={(e) => setValues({ ...values, name: e.target.value })}
+					/>
+					<datalist id="ingredients">
+						<select>{ingredientList}</select>
+					</datalist>
+					<Form.Control
+						as="select"
+						className="form-component"
+						value={values.unit}
+						name="unit"
+						placeholder="Unit"
+						onChange={(e) => setValues({ ...values, unit: e.target.value })}
+					>
+						<option value="" selected disabled hidden>
+							Pick a unit
+						</option>
+						{unitList}
+					</Form.Control>
+					<Form.Control
+						class="form-component"
+						value={values.quantity}
+						name="quantity"
+						placeholder="Quantity"
+						type="number"
+						onChange={(e) => setValues({ ...values, quantity: e.target.value })}
+					/>
+					<Form.Control
+						class="form-component"
+						value={values.expiry}
+						name="expiry"
+						placeholder="Expiration Date (Optional)"
+						type="date"
+						onChange={(e) => setValues({ ...values, expiry: e.target.value })}
+					/>
+					<Button variant="info" type="submit" onClick={(event) => props.handleAddItem(event, values)}>
+						Add
+					</Button>
+				</Form.Group>
+			</Form>
+		</Fragment>
+	);
+}
