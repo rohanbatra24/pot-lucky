ff8cfa21b90e0ba51ac0bced2a5266a9a138a9a0
Rohan Batra <rohanbatra@Rohans-MacBook-Pro.local>
2020-05-14 17:18:47 -0400

    add recipe modal

--- src/components/Recipes/Show.js
+++ src/components/Recipes/Show.js
@@ -1,28 +1,52 @@
 import React, { useState, Fragment } from 'react';
 import classNames from 'classnames';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 import FullRecipe from './FullRecipe';
-
+import '../../App.css';
 
 export default function Show(props) {
+	const [ show, setShow ] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
 	const pantryClass = classNames({
 		'pantry-list__item'           : props,
 		'pantry-list__item--selected' : props.selected
 	});
 
-
-	if (props.selected === props.recipe.id) {
-		return (
-			<FullRecipe selected={props.selected} recipe={props.recipe}/> 
-		)
-	} else {
-		return (
-			<Fragment>
+	return (
+		<Fragment>
 			<ListGroup.Item as="li">
 				<h3>{props.recipe.title}</h3>
-				<button onClick={() => props.setSelected(props.recipe.id)}> View Full Recipe</button>
+				<button
+					onClick={() => {
+						props.setSelected(props.recipe.id);
+						handleShow();
+					}}
+				>
+					View Full Recipe
+				</button>
 			</ListGroup.Item>
-			</Fragment>
-		);
-	}
+
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>Full Recipe</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<FullRecipe recipe={props.recipe} />
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleClose}>
+						Close
+					</Button>
+					<Button variant="primary" onClick={handleClose}>
+						Make this and update my pantry!
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</Fragment>
+	);
 }
