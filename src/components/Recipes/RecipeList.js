224df67ce0511032fea9a953732bfb0a9948a448
Rohan Batra <rohanbatra@Rohans-MacBook-Pro.local>
2020-05-14 20:19:44 -0400

    add className

--- src/components/Recipes/RecipeList.js
+++ src/components/Recipes/RecipeList.js
@@ -5,33 +5,32 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import './styles.css';
 
 export default function RecipeList(props) {
-	const [selected, setSelected] = useState("")
+	const [ selected, setSelected ] = useState('');
 
-	useEffect(() => {
-		makeRecipeList();
-	}, [selected, props.recipes])
+	useEffect(
+		() => {
+			makeRecipeList();
+		},
+		[ selected, props.recipes ]
+	);
 
 	// Retrieve list from database
 	// First item in list will always be New/Form
 	// Render PantryListItem in each iteration
 	const makeRecipeList = () => {
 		if (props.recipes.length === 0) {
-			return (
-				<div> Empty List - Search for something or add to your mixing bowl! </div>
-			)
-		} else {
-			const recipes = props.recipes.map(recipe => {
-				return (
-					<Show selected={selected} setSelected={setSelected} recipe={recipe} />
-					)
-				})
-				return recipes;
-			}
-	}
-
+			return <div> Empty List - Search for something or add to your mixing bowl! </div>;
+		}
+		else {
+			const recipes = props.recipes.map((recipe) => {
+				return <Show selected={selected} setSelected={setSelected} recipe={recipe} />;
+			});
+			return recipes;
+		}
+	};
 
 	return (
-		<ListGroup as="ul">
+		<ListGroup className="overflow-auto" as="ul">
 			{makeRecipeList()}
 		</ListGroup>
 	);
