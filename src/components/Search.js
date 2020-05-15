ef0925651d905e444242665c5dd19b9585ddfa12
Kelsey Griffin <kjulegriffin@gmail.com>
2020-05-15 13:04:15 -0400

    WIP - try to use spoonacular key from env

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,10 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import axios from 'axios';
-import bulkInfo from '../assets/api_bulk_info';
-import getBulkInfo from '../assets/api_bulk_info';
 
-// API KEYS
-// 7a707a8f3c6b42ffb52bccfa111f4a00
-// 4ed5da45f1c94518a9663b95f895c3b3
-// 692296bbb58c433b89dba0eb2c54099b
-// dc75b7fa4f7948ceae6ce8c4e008208e
-// b183b79d7863488aaca6c2aeb086183b
-// 76961f7e03aa4b47955a3ce3c98c45f6
-// a59c64c98d4d4156b1e8d2992bc5ca06
-// 0939358abf5240f598df923e8a915144
-// a260062916e04970801d03d0db2c32b4
-// 46ef327a373243b192fc86af3fa94823 {{{IN-USE}}}
+console.log("api key: ", process.env.SPOONACULAR_API_KEY)
+// const apiKey = process.env.SPOONACULAR_API_KEY;
+const apiKey = "b183b79d7863488aaca6c2aeb086183b"
 
-const apiKey = 'dc75b7fa4f7948ceae6ce8c4e008208e';
 export default function Search(props) {
 	const [ searchText, setSearchText ] = useState('');
 
